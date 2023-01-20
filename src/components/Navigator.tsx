6fa1790eab3847538b91bc55d360adb585942b79
me@zatar.dev <me@zatar.dev>
2023-01-20 17:28:48 +0300

    Create navigator component

---
+++ src/components/Navigator.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {
+  BottomNavigation,
+  BottomNavigationTab,
+  Icon,
+} from '@ui-kitten/components';
+import {SCREENS} from '../constants/screens';
+
+function Navigator({navigation, state}): JSX.Element {
+  const currentScreen = state.index;
+
+  return (
+    <BottomNavigation
+      style={styles.bottomNavigation}
+      selectedIndex={currentScreen}
+      onSelect={index =>
+        navigation.navigate(index === 0 ? SCREENS.DASHBOARD : SCREENS.MORE)
+      }>
+      <BottomNavigationTab title="Dashboard" icon={<Icon name="home" />} />
+      <BottomNavigationTab
+        title="More"
+        icon={<Icon name="more-horizontal" />}
+      />
+    </BottomNavigation>
+  );
+}
+
+export default Navigator;
+
+const styles = StyleSheet.create({
+  bottomNavigation: {
+    marginVertical: 8,
+    position: 'absolute',
+    bottom: 0,
+  },
+});
--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -1,30 +1,31 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
+import Navigator from '../components/Navigator';
 import {SCREENS} from '../constants/screens';
 import DashboardScreen from '../screens/Dashboard';
 import MoreScreen from '../screens/More';
 import RegistrationScreen from '../screens/Registration';
 import SplashScreen from '../screens/Splash';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator<any>();
 
 function MainNavigation(): JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={SCREENS.SPLASH}>
-        <Stack.Screen
+      <Tab.Navigator tabBar={props => <Navigator {...props} />}>
+        <Tab.Screen name={SCREENS.DASHBOARD} component={DashboardScreen} />
+        <Tab.Screen name={SCREENS.MORE} component={MoreScreen} />
+        <Tab.Screen
           name={SCREENS.SPLASH}
           component={SplashScreen}
           options={{headerShown: false}}
         />
-        <Stack.Screen
+        <Tab.Screen
           name={SCREENS.REGISTRATION}
           component={RegistrationScreen}
         />
-        <Stack.Screen name={SCREENS.DASHBOARD} component={DashboardScreen} />
-        <Stack.Screen name={SCREENS.MORE} component={MoreScreen} />
-      </Stack.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
