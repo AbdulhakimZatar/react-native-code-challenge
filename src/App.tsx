05208d15cc1b404fe04eaea3721ee758d0c243f7
me@zatar.dev <me@zatar.dev>
2023-01-22 11:19:25 +0300

    Add safe area view

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import * as eva from '@eva-design/eva';
 import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
+import {SafeAreaView, StyleSheet} from 'react-native';
 
 import {theme} from './theme';
 import MainNavigation from './navigation/MainNavigator';
@@ -10,13 +11,19 @@ import './i18n';
 
 function App(): JSX.Element {
   return (
-    <>
+    <SafeAreaView style={styles.safeArea}>
       <IconRegistry icons={EvaIconsPack} />
       <ApplicationProvider {...eva} theme={theme}>
         <MainNavigation />
       </ApplicationProvider>
-    </>
+    </SafeAreaView>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+});
