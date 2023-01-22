d6515ceb9ba1bf81ad549bd138138c69d581a316
me@zatar.dev <me@zatar.dev>
2023-01-22 08:48:45 +0300

    Dashboard - swipe down to refresh

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,32 +1,66 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Layout, List, Text} from '@ui-kitten/components';
-import {I18nManager, StyleSheet} from 'react-native';
+import {I18nManager, RefreshControl, StyleSheet} from 'react-native';
 import {dashboardData} from '../data/dashboard';
 import moment from 'moment';
 
 function DashboardScreen() {
-  const renderItem = ({item: buyers, index}) => {
+  const [refreshing, setRefreshing] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData(dashboardData);
+      setRefreshing(false);
+    }, 750);
+  }, []);
+
+  const resetData = () => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setData(dashboardData);
+      setRefreshing(false);
+    }, 750);
+  };
+
+  const renderItem = ({item: buyers, index: i}) => {
     return (
       <Layout
         style={{
           ...styles.item,
-          ...(index === 0 ? styles.firstItem : {}),
+          ...(i === 0 ? styles.firstItem : {}),
         }}>
         <Text style={styles.date} category="s1">
           {moment(buyers[0].date, 'YYYY-MM-DD').fromNow()}
         </Text>
-        {buyers.map((buyer, index) => (
-          <Text style={styles.name} key={index}>
-            {buyer.name} transferred {buyer.paid}$
+        {buyers.map((buyer, j) => (
+          <Text style={styles.name} key={j}>
+            {buyer.name} transferred {buyer.amount}$
           </Text>
         ))}
       </Layout>
     );
   };
 
+  const emptyList = () => {
+    return (
+      <Text style={styles.container} category="h6">
+        No data available
+      </Text>
+    );
+  };
+
   return (
     <Layout style={styles.container}>
-      <List style={styles.list} data={dashboardData} renderItem={renderItem} />
+      <List
+        style={styles.list}
+        data={data}
+        renderItem={renderItem}
+        ListEmptyComponent={emptyList}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={resetData} />
+        }
+      />
     </Layout>
   );
 }
@@ -38,6 +72,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    textAlign: 'center',
   },
   list: {
     width: '100%',
